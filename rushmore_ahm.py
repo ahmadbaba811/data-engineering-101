493b1f28a39c513ac8e329a21ed556b8307b9d3c
ahmadbaba811 <ahmadbaba811@hotmail.com>
2025-07-11 21:18:23 +0100

    added assignment features

--- rushmore_ahm.py
+++ rushmore_ahm.py
@@ -1,6 +1,8 @@
 import json # simple database for storing orders
 from datetime import datetime # for timestamping orders
 import os # for checking if the order file exists
+from collections import Counter
+
 
 
 # Pizza Information
@@ -11,12 +13,13 @@ pizza_data = {
     "4": {"name": "Deluxe", "price": 6.0},
     "5": {"name": "Vegetable", "price": 4.0},
     "6": {"name": "Chocolate", "price": 12.0},
-    "7": {"name": "Cheese", "price": 5.0}
+    "7": {"name": "Cheese", "price": 5.0},
+    "8": {"name": "BBQ Beef", "price": 5.5} #TASK 1
 }
 
 ORDER_DB_FILE = "pizza_orders.json"
 
-def save_order_to_json(pizza_name, quantity, total_price, order_type, discount_applied):
+def save_order_to_json(customer_name, pizza_name, quantity, total_price, order_type, discount_applied):
     """
     Append a new order to the JSON 'database' file.
     If the file does not exist, it creates a new one.
@@ -33,7 +36,8 @@ def save_order_to_json(pizza_name, quantity, total_price, order_type, discount_a
         "order_type": order_type,
         "quantity": quantity,
         "total_price": round(total_price, 2),
-        "discount_applied": discount_applied
+        "discount_applied": discount_applied,
+        "customer_name": customer_name
     }
 
     if os.path.exists(ORDER_DB_FILE):
@@ -63,6 +67,7 @@ def calculate_payment(price, quantity, discount_rate=0.0):
     discount = total * discount_rate
     return total - discount
 
+
 # Handle box and slice orders with discounts
 def handle_box_order(pizza_name, price):
     """
@@ -85,14 +90,36 @@ def handle_box_order(pizza_name, price):
     elif quantity >= 5:
         discount_rate = 0.10 # 10% discount for 5 or more boxes
 
-    discount_applied = discount_rate > 0.0
-    total = calculate_payment(price, quantity, discount_rate)
-
-    print(f"Your payment is ${total:.2f} for {quantity} box(es).")
+    discount_applied = True if discount_rate > 0.0 else False
     if discount_applied:
-        print(f"A discount of {int(discount_rate * 100)}% was applied.")
+        total = calculate_payment(price, quantity, discount_rate)
+        # TASK 2: Apply 7.5% tax
+        tax_amount = round((7.5/100)*total, 2)
+        taxed_total = tax_amount + total
+        print(f"Total before tax is: {total}")
+        print(f"A tax of {tax_amount} was applied")
+        print(f"Your total with tax is {taxed_total:.2f}")
 
-    save_order_to_json(pizza_name, quantity, total, "box", discount_applied)
+#    print(f"Your payment is ${total:.2f} for {quantity} box(es).")
+#   if discount_applied:
+#      print(f"A discount of {int(discount_rate * 100)}% was applied")
+
+# TASK 4: Ask for customer name
+    customer_name = input("Great, please what's your name?: ")
+
+# TASK 3
+    print(f"""
+--Your order is successful--
+-----ORDER SUMMARY-----
+Customer   --- {customer_name}
+Pizza Name --- {pizza_name}
+Quantity   --- {quantity}
+Discount:  --- {discount_rate}
+Tax Applied--- {tax_amount}
+Total to pay-- {taxed_total:.2f}
+            """)
+
+    save_order_to_json(customer_name, pizza_name, quantity, total, "box", discount_applied)
 
 
 def handle_slice_order(pizza_name, slice_price):
@@ -159,6 +186,7 @@ def main():
         print("\nOptions:")
         print("Enter the number to order a pizza")
         print("Type 'q' to quit")
+        print("Type s to view stats")
 
         choice = input("What would you like to do? ").strip().lower()
 
@@ -169,9 +197,54 @@ def main():
             view_orders()
         elif choice in pizza_data:
             order_pizza(choice)
+        elif choice == 's':
+            view_stats()
         else:
             print("Invalid input. Please try again.")
 
+def view_stats():
+    """View order statistics"""
+    if os.path.exists(ORDER_DB_FILE):
+        with open(ORDER_DB_FILE, "r", encoding='utf-8') as file:
+            try:
+                orders = json.load(file)
+                if not orders:
+                    print("Hey!, no orders yet")
+                else:
+                    # TOTAL ORDERS
+                    total_orders = len(orders)
+
+                    # TOTAL PIZZA COUNT
+                    pizza_counts = {}
+                    for order in orders:
+                        pizza = order["pizza_type"]
+                        if pizza in pizza_counts:
+                            pizza_counts[pizza] += 1
+                        else:
+                            pizza_counts[pizza] = 1
+
+                    most_frequent_pizza = None
+                    max_count = 0
+                    for pizza, count in pizza_counts.items():
+                        if count > max_count:
+                            max_count = count
+                            most_frequent_pizza = pizza
+
+                    # TOTAL REVENUE
+                    total_revenue = sum(order["total_price"] for order in orders)
+
+                    print(f"""
+-----ORDER STATISTICS-----
+Total Orders    --- {total_orders}
+Frequent Orders --- {most_frequent_pizza}
+total_revenue   --- {total_revenue}
+                          """
+                    )
+
+            except json.JSONEncoder:
+                print("Error reading the order database")
+    else:
+        print("No orders yet")
 
 def view_orders():
     """
